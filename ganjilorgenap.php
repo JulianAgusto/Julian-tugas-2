7eb3e92b63181deb63f0fb3d229ac87d6024aab6
JulianAgusto <127101946+JulianAgusto@users.noreply.github.com>
2023-03-06 21:08:41 +0900

    Add files via upload

---
+++ ganjilorgenap.php
@@ -0,0 +1,8 @@
+<?php
+
+$bil = 7;
+
+$ket = ($bil % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
+
+echo "$ket" . PHP_EOL;
+ 
---
+++ switcstatment.php
@@ -0,0 +1,27 @@
+<?php
+
+$harba ="c";
+
+switch ($harba) {
+    case 'a':
+        echo "harga barang ini 7.000".PHP_EOL;
+
+        break;
+        case 'b':
+        echo "harga barang ini 2.500" .PHP_EOL;
+        break;
+        case 'c':
+        echo "harga barang ini 10.000" .PHP_EOL;
+        break;
+        case 'd':
+        echo "harga barang ini 5.500" .PHP_EOL;
+        break;
+        case 'e':
+        echo "harga barang ini php6.000" .PHP_EOL;
+        break;
+
+        default:
+        echo "Harga Tidak Ditemukan..!!" .PHP_EOL;
+
+        break;
+}
